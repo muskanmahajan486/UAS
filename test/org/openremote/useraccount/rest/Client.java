10326af7e1a18ad3d98722ebe1d54d5425599c84
Marcus Redeker <marcus@openremote.org>
2012-05-25 22:15:14 +0000

    renamed old package and cleaned test class
    git-svn-id: https://openremote.svn.beanstalkapp.com/beehive/workspace/mredeker/UserAccountService@118 2bd35c9c-1000-49a5-b779-596279832df3

---
+++ test/org/openremote/useraccount/rest/Client.java
@@ -0,0 +1,22 @@
+package org.openremote.useraccount.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openremote.devicediscovery.domain.DiscoveredDeviceAttrDTO;
+import org.openremote.devicediscovery.domain.DiscoveredDeviceDTO;
+import org.openremote.rest.GenericResourceResultWithErrorMessage;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ClientResource;
+
+import flexjson.JSONDeserializer;
+import flexjson.JSONSerializer;
+
+public class Client
+{
+//TODO
+}
