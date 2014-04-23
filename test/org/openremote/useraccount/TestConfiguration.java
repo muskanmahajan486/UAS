5d2e18d441f2e9b1314a00f8b8f7611b8d9e9248
Eric Bariaux <eric@openremote.org>
2014-04-23 14:04:21 +0000

    Merged MODELER-532, r273:306
    git-svn-id: https://openremote.svn.beanstalkapp.com/beehive/workspace/ebariaux/UserAccountService_ProInstaller_1_0_13@307 2bd35c9c-1000-49a5-b779-596279832df3

---
+++ test/org/openremote/useraccount/TestConfiguration.java
@@ -0,0 +1,8 @@
+package org.openremote.useraccount;
+
+public interface TestConfiguration {
+
+	static final String UAS_BASE_REST_URL = "http://localhost:8090/uas/rest/";
+	static final String ACCOUNT_MANAGER_USER = "designer_appl";
+	static final String ACCOUNT_MANAGER_PASSWORD = "password";
+}
